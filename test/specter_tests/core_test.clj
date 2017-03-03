0b2721e4c1dafa2a992d4279e5d26034d8108f9e
Bill Tozier <bill@vagueinnovation.com>
2017-03-03 13:15:53 -0500

    extracted mutation function

--- test/specter_tests/core_test.clj
+++ test/specter_tests/core_test.clj
@@ -34,5 +34,52 @@
       (provided (rand) =streams=> (cycle [0.1 0.9 0.9]))
     )
 
+  (fact "I can also change the items by jamming in values"
+    (setval
+      [(walker #(and (integer? %) (< (rand) 0.3)))]
+      99
+      deep-tree) => [99 [2 3] [99 [] 5 [6 [99] 8] 9 [[[99 11] 12] 99]]]
+      ;;             1   2 3    4    5  6   7  8  9    10 11  12  13
+      (provided (rand) =streams=> (cycle [0.1 0.9 0.9]))
+    )
 
+  (fact "I can change the FIRST of those if I want"
+    (transform
+      [FIRST (walker #(and (integer? %) (< (rand) 0.3)))]
+      (constantly 1111)
+      deep-tree) => [1111 [2 3] [4 [] 5 [6 [7] 8] 9 [[[10 11] 12] 13]]]
+      ;;             1     2 3   4    5  6  7  8  9    10 11  12  13
+      (provided (rand) =streams=> (cycle [0.1 0.9 0.9]))
+    )
+    )
+
+
+  (fact "I can change the FIRST of those if I want"
+    (transform
+      [FIRST (walker integer?)]
+      (constantly 1111)
+      deep-tree) => [1111 [2 3] [4 [] 5 [6 [7] 8] 9 [[[10 11] 12] 13]]]
+      ;;             1     2 3   4    5  6  7  8  9    10 11  12  13
+    )
+
+
+(def TREE-VALUES
+  (recursive-path [] p
+    (if-path vector?
+      [ALL p]
+      STAY)))
+
+(defn mutate-one-value
+  [changeable-values]
+  (assoc
+    changeable-values                       ;; a flat collection, here
+    (rand-int (count changeable-values))    ;; index of an item in the collection
+    1111))                                  ;; new value assigned to that index
+
+(fact "trying Marz's solution"
+  (transform
+    (subselect TREE-VALUES) ;; gather all the values currently selected by TREE-VALUES
+    mutate-one-value
+    deep-tree) => [1 [2 3] [4 [] 1111 [6 [7] 8] 9 [[[10 11] 12] 13]]]
+    (provided (rand-int 13) => 4) ;; the "random item" will always be item #4
     )
